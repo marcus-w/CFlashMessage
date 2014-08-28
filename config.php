f1488bc08822b13c542932d85398e67d273c65ee
marcus-w <mwesterl@kth.se>
2014-08-28 16:38:56 +0200

    Update 29

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
  * Define essential Anax paths, end with /
  *
  */
-define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../../../') . '/');
+define('ANAX_INSTALL_PATH', realpath(__DIR__ . '../../../') . '/');
 define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . '/app/');
 
 
