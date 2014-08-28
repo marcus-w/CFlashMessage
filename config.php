ec391cb4efbc4ed41d9e9a2c30b7967cdd02d3be
marcus-w <mwesterl@kth.se>
2014-08-28 19:01:03 +0200

    5

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
  * Define essential Anax paths, end with /
  *
  */
-define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../../../') . '/');
+define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../../../'));
 define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . '/app/');
 
 
@@ -18,7 +18,7 @@ define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . '/app/');
  * Include autoloader.
  *
  */
-include(ANAX_APP_PATH . 'app/config/autoloader.php'); 
+include(ANAX_APP_PATH . 'config/autoloader.php');
 
 
 
@@ -26,5 +26,4 @@ include(ANAX_APP_PATH . 'app/config/autoloader.php');
  * Include global functions.
  *
  */
-include(ANAX_INSTALL_PATH . 'src/functions.php'); 
-
+include(ANAX_INSTALL_PATH . 'src/functions.php');
