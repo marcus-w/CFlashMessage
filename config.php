4400ffe23adb3f43da2a3a4b6d4e91d99bad0ce9
marcus-w <mwesterl@kth.se>
2014-08-28 09:43:39 +0200

    Update 16

--- config.php
+++ config.php
@@ -10,7 +10,7 @@
  *
  */
 define('ANAX_INSTALL_PATH', realpath(__DIR__ . '/../../../') . '/');
-define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . '/../../app/');
+define('ANAX_APP_PATH',     ANAX_INSTALL_PATH . '/app/');
 
 
 
--- test/CFlashMessage/FlashTest.php
+++ test/CFlashMessage/FlashTest.php
@@ -8,49 +8,56 @@ class FlashTest extends \PHPUnit_Framework_TestCase
 {
 public function setup()
 {
-    $this->flash = new CFlashMessage();
+$this->flash = new CFlashMessage();
 }
 public function testClear()
 {
-    $this->flash->clear();
-    $this->assertEquals(null, $_SESSION["flash"]);
+$this->flash->message('info', 'Info type message');
+$this->assertEquals('info', $_SESSION['flash'][0]['type']);
+$this->flash->clear();
+$this->assertEquals(null, $_SESSION['flash']);
 }
-public function testAdd()
+public function testMessage()
 {
-    $this->flash->add('info', 'Info message');
-    $this->flash->add('success', 'Success message');
-    $this->flash->add('warning', 'Warning message');
-    $this->flash->add('error', 'Error message');
-    $this->flash->add('random inavlid type', 'This will become a info message as default');
-    $this->assertEquals('info', $_SESSION['flash'][0]['type']);
-    $this->assertEquals('success', $_SESSION['flash'][1]['type']);
-    $this->assertEquals('warning', $_SESSION['flash'][2]['type']);
-    $this->assertEquals('error', $_SESSION['flash'][3]['type']);
-    $this->assertEquals('Info message', $_SESSION['flash'][0]['msg']);
-    $this->assertEquals('Success message', $_SESSION['flash'][1]['msg']);
-    $this->assertEquals('Warning message', $_SESSION['flash'][2]['msg']);
-    $this->assertEquals('Error message', $_SESSION['flash'][3]['msg']);
+$this->flash->message('info', 'Info example message');
+$this->flash->message('success', 'Success example message');
+$this->flash->message('warning', 'Warning example message');
+$this->flash->message('error', 'Error example message');
+$this->assertEquals('info', $_SESSION['flash'][0]['type']);
+$this->assertEquals('success', $_SESSION['flash'][1]['type']);
+$this->assertEquals('warning', $_SESSION['flash'][2]['type']);
+$this->assertEquals('error', $_SESSION['flash'][3]['type']);
+$this->assertEquals('Info example message', $_SESSION['flash'][0]['message']);
+$this->assertEquals('Success example message', $_SESSION['flash'][1]['message']);
+$this->assertEquals('Warning example message', $_SESSION['flash'][2]['message']);
+$this->assertEquals('Error example message', $_SESSION['flash'][3]['message']);
 }
-public function testGet()
+public function testSpecificMessageFunctions()
 {
-    $this->flash->add('info', 'Info message');
-    $output = $this->flash->get();
-    $expected = "<div class='flash_info'>\n";
-    $expected .= "\t" . 'Info message' . "\n</div>\n";
-    $this->assertEquals($expected, $output);
+$this->flash->info('Info example message');
+$this->flash->success('Success example message');
+$this->flash->warning('Warning example message');
+$this->flash->error('Warning example message');
+$this->assertEquals('info', $_SESSION['flash'][0]['type']);
+$this->assertEquals('success', $_SESSION['flash'][1]['type']);
+$this->assertEquals('warning', $_SESSION['flash'][2]['type']);
+$this->assertEquals('error', $_SESSION['flash'][3]['type']);
+$this->assertEquals('Info example message', $_SESSION['flash'][0]['message']);
+$this->assertEquals('Success example message', $_SESSION['flash'][1]['message']);
+$this->assertEquals('Success example message', $_SESSION['flash'][2]['message']);
+$this->assertEquals('Error example message', $_SESSION['flash'][3]['message']);
 }
-public function testGetWithIcon()
+public function testGetMessages()
 {
-    $this->flash->add('info', 'Info message');
-    $output = $this->flash->get('icons');
-    $expected = "<div class='flash_info'>\n";
-    $expected .= "\t<i class='fa fa-info-circle'></i>\n";
-    $expected .= "\t" . 'Info message' . "\n</div>\n";
-    $this->assertEquals($expected, $output);
+$this->flash->clear();
+$this->flash->message('info', 'Info example message');
+$expectedResult = "<div class='flash_info'>\n";
+$expectedResult .= "\t" . "Info example message" . "\n</div>\n";
+$this->assertEquals($expectedResult, $this->flash->getMessages());
 }
-public function testGetEmptyMessages()
+public function testEmptyGetMessages()
 {
-    $output = $this->flash->getMessage();
-    $this->assertEquals(null, $output);
+$this->flash->clear();
+$this->assertEquals(null, $this->flash->getMessages());
 }
 }
